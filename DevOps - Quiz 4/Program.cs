c60adde3ea0193be3b0a2c4b08d753a46f65e69d
longquan98@gmail.com <longquan98@gmail.com>
2024-03-28 14:09:38 -0400

    Migrating database to the cloud

--- DevOps - Quiz 4/Program.cs
+++ DevOps - Quiz 4/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine
     ("Database access has already been done " +
     "on April 14, 2023 by Hoang Long Le");
+    
+Console.WriteLine('Migrating local databse to cloud successfully!');
